2b7a6f1c07883287948c00d35a1923463fccb623
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-03-29 14:04:46 +0200

    fix(react): testing with react testing library and storybook

--- files/react/setupTests.ts
+++ files/react/setupTests.ts
@@ -1,10 +1,10 @@
-import { setGlobalConfig } from "@storybook/testing-react";
+import { setProjectAnnotations, cleanup } from "@storybook/testing-react";
 
 /* eslint-disable @typescript-eslint/no-namespace */
 import matchers, {
   TestingLibraryMatchers,
 } from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
+import { expect, afterEach } from "vitest";
 
 // Storybook's preview file location
 import globalStorybookConfig from "./.storybook/preview";
@@ -18,6 +18,8 @@ declare global {
   }
 }
 
-setGlobalConfig(globalStorybookConfig);
+afterEach(cleanupt;
+
+setProjectAnnotations(globalStorybookConfig);
 
 expect.extend(matchers);
