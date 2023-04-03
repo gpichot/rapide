f193e10ec51e839daede5335169c7a94ac9080f7
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-04-03 06:24:45 +0200

    fix: typos

--- files/react/App.test.tsx
+++ files/react/App.test.tsx
@@ -1,6 +1,5 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
@@ -8,7 +7,6 @@ describe("App", () => {
   it("renders App component", () => {
     render(<App />);
 
-    expect(screen.getByText(/Hello World/)).toBeVisible()
+    expect(screen.getByText(/Hello World/)).toBeVisible();
   });
 });
-
--- files/react/setupTests.ts
+++ files/react/setupTests.ts
@@ -1,10 +1,11 @@
-import { setProjectAnnotations, cleanup } from "@storybook/testing-react";
+import { setProjectAnnotations } from "@storybook/testing-react";
 
 /* eslint-disable @typescript-eslint/no-namespace */
 import matchers, {
-  TestingLibraryMatchers,
+  TestingLibraryMatchers
 } from "@testing-library/jest-dom/matchers";
 import { expect, afterEach } from "vitest";
+import { cleanup } from '@testing-library/react';
 
 // Storybook's preview file location
 import globalStorybookConfig from "./.storybook/preview";
@@ -18,7 +19,7 @@ declare global {
   }
 }
 
-afterEach(cleanupt;
+afterEach(cleanup);
 
 setProjectAnnotations(globalStorybookConfig);
 
