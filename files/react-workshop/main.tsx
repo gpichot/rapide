b928d2c99bce5b8f033d3b8a5336316a39f6b5e0
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-04-03 07:39:44 +0200

    fix: react-workshop

--- files/react-workshop/main.tsx
+++ files/react-workshop/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import "./index.css";
-
 import App from "./App";
 
 const queryClient = new QueryClient();
