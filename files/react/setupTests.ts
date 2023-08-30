e65581d44d1218e2c45e0602bfd55bceb672a408
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-08-31 01:29:09 +0200

    feat: use pnpm

--- files/react/setupTests.ts
+++ files/react/setupTests.ts
@@ -2,20 +2,11 @@ import { setProjectAnnotations } from "@storybook/testing-react";
 
 /* eslint-disable @typescript-eslint/no-namespace */
 import "@testing-library/jest-dom/vitest";
-import { expect, afterEach } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 
 // Storybook's preview file location
 import globalStorybookConfig from "./.storybook/preview";
-
-declare global {
-  namespace Vi {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    interface JestAssertion<T = any>
-      extends jest.Matchers<void, T>,
-        TestingLibraryMatchers<T, void> {}
-  }
-}
 
 afterEach(cleanup);
 
