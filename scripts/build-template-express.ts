0f65f1b3950d7a0beceb532f8f42984a55e8c54e
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-08-31 02:57:19 +0200

    feat: use pnpm

--- scripts/build-template-express.ts
+++ scripts/build-template-express.ts
@@ -17,7 +17,7 @@ async function buildTemplateExpress({
 
   // Create the project
   await fs.mkdir(buildDir);
-  await runCommand("pnpm", ["init", "-y"], commandOptions);
+  await runCommand("pnpm", ["init"], commandOptions);
 
   // Add dependencies
   templateBuilder.packageJSON.addDevDependencies([
